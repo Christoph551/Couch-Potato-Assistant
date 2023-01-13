985450677e1d3217dc567180f8595c5cd811dc0c
lsegura06 <leosegura006@gmail.com>
2023-01-13 11:27:33 -0700

    index&Java

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const omdbKey = '8b30258'
 
 
 //variables to tie the html
-const searchInput = $('#searchInput'); // On index.html page
 const searchBtn = $('#searchBtn'); // On index.html page
 const homeBtn = $('#homeBtn'); // On summary.html page
 const movieResults = $('#movieResults'); // On summary.html page
 const moviePhoto = $('moviePhoto');
+const $movieName = $("#movieName")
 // const windEl = $('#wind');
 // const cityListEl = $('.cityList');
 // const cityInput = $('#city-input');
@@ -25,16 +25,16 @@ $(document).ready(function () {
 
 //makes it not case sensitive 
 function compare(a, b) {
-return a.Movie.toUpperCase().localeCompare(b.Movie.toUpperCase());
+return a.movieName.toUpperCase().localeCompare(b.movieName.toUpperCase());
 }
 
 
 
 // local storage for movie
 function loadMovies() {
-    const storedCities = JSON.parse(localStorage.getItem('pastCities'));
-    if (storedCities) {
-        pastCities = storedCities;
+    const storedMovie = JSON.parse(localStorage.getItem('pastMovie'));
+    if (storedMovie) {
+        pastMovie = storedMovie;
     }
 }
 
@@ -43,26 +43,44 @@ function storeMovies() {
     localStorage.setItem('pastMovies', JSON.stringify(pastMovies));
 }
 
+searchBtn.click(function() {
+    const movieName = $("#movieName").val();
+    $.ajax({
+        url: `http://www.omdbapi.com/?apikey=${omdbKey}&t=${movieName}`,
+        data: {
+                    name: movieName
+                },
+                
+                success: function(data) {
+                console.log(movieName);    // process the returned data
+                    window.location.href = "summary.html?name=" + movieName;
+                }
+                
+            });
+        });
+
+    })
+
 
 // Functions for the movie, streaming api calls
 
-function buildURLcity(city) {
-    if (city) {
-        return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    }
-}
-
-//function buildUrlMovie(Movie){
-// if (movie){
-//      return "http://www.omdbapi.com/?apikey=${ombdKey}&t=${Movie}"   
-//}
+//function buildURLcity(city) {
+    //if (city) {
+  //      return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+//   }
 //}
 
-//funtion buildUrlMovImg(Poster){ this one is ???
-// if (poster){
-//   return "http://img.omdbapi.com/?apikey=${omdbKey}&t=${Movie}";  
-//}
-//}
+// function buildUrlMovie(Movie){
+//     if (Movie){
+//     return `http://www.omdbapi.com/?apikey=${ombdKey}&t=${Movie}`;   
+// }
+// }
+
+// function buildUrlMovImg(poster){ ///this one is ???
+//     if (poster){
+//     return `http://img.omdbapi.com/?apikey=${omdbKey}&t=${Movie}`;
+// }
+// }
 
 //const options = { //this is the stream fetch command 
 	//method: 'GET',
@@ -83,56 +101,44 @@ function buildURLcity(city) {
 //
 //
 
-function buildURLId(id) {
-    return `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}`;
+// function buildURLId(id) {
+//     return `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}`;
 
-})
+// })
 
-            // Store current city in past cities // make it into movies
-            let city = response.name;
-            let id = response.id;
-            // Remove duplicate cities
-            if (pastCities[0]) {
-                pastCities = $.grep(pastCities, function (storedCity) {
-                    return id !== storedCity.id;
-                })
-            }
-            pastCities.unshift({ city, id });
-            storeCities();
-            displayCities(pastCities);
-            
-            // Display current weather // make it into movie 
-            cityEl.text(response.name);
-            let formattedDate = moment.unix(response.dt).format('L');
-            dateEl.text(formattedDate);
-            let weatherIcon = response.weather[0].icon;
-            weatherIconEl.attr('src', `http://openweathermap.org/img/wn/${weatherIcon}.png`).attr('alt', response.weather[0].description);
-            temperatureEl.html(((response.main.temp - 273.15) * 1.8 + 32).toFixed(1));
-            humidityEl.text(response.main.humidity);
-            windEl.text((response.wind.speed * 2.237).toFixed(1));
+// Store current city in past cities // make it into movies
+// let city = response.name;
+// let id = response.id;
+// // Remove duplicate cities
+// if (pastCities[0]) {
+//     pastCities = $.grep(pastCities, function (storedCity) {
+//         return id !== storedCity.id;
+//     })
+// }
+// pastCities.unshift({ city, id });
+// storeCities();
+// displayCities(pastCities);
 
-            function displayLastSearchedCity() {
-                if (pastCities[0]) {
-                    let queryURL = buildURLId(pastCities[0].id);
-                    searchWeather(queryURL);
-                } else {
-                    // if no past searched cities, load Detroit weather data
-                    let queryURL = buildURLcity("Tucson");
-                    searchWeather(queryURL);
-                }
-            }
+// // Display current weather // make it into movie 
+// cityEl.text(response.name);
+// let formattedDate = moment.unix(response.dt).format('L');
+// dateEl.text(formattedDate);
+// let weatherIcon = response.weather[0].icon;
+// weatherIconEl.attr('src', `http://openweathermap.org/img/wn/${weatherIcon}.png`).attr('alt', response.weather[0].description);
+// temperatureEl.html(((response.main.temp - 273.15) * 1.8 + 32).toFixed(1));
+// humidityEl.text(response.main.humidity);
+// windEl.text((response.wind.speed * 2.237).toFixed(1));
 
-            $('#search-btn').on('click', function (event) {
-                // prevents refresh page
-                event.preventDefault();
-        
-        
-                // Clear the input fields
-                cityInput.val('');
-        
-                // Build the query url with the city and searchWeather // make it movie
-                if (city) {
-                    let queryURL = buildURLcity(city);
-                    searchWeather(queryURL);
-                }
-            }); 
+// function displayLastSearchedCity() {
+//     if (pastCities[0]) {
+//         let queryURL = buildURLId(pastCities[0].id);
+//         searchWeather(queryURL);
+//     } else {
+//         // if no past searched cities, load Detroit weather data
+//         let queryURL = buildURLcity("Tucson");
+//         searchWeather(queryURL);
+//     }
+// }
+
+
+
