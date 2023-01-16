12a3e3d3403616f36c7e20f69312e3c5e6464ee2
Christoph551 <christophersimmonds551@gmail.com>
2023-01-16 01:53:40 -0700

    Corrected image size. Added modal.

--- summary.js
+++ summary.js
@@ -1,23 +1,7 @@
 // Variables moved from script.js file
-
-
-
-
-
 const movieName = new URLSearchParams(window.location.search).get("name");
 const movieData = JSON.parse(localStorage.getItem(movieName));
 
 document.querySelector("#movieTitle").textContent = movieData.Title;
 document.querySelector("#moviePlot").textContent = movieData.Plot;
 document.querySelector("#moviePoster").src = movieData.Poster;
-
-
-// const $movieResults = $('#movieResults'); 
-// let pastMovies = []; 
-
-
-
-const movieData = JSON.parse(localStorage.getItem("Shrek")); 
-const actors = movieData.Actors;
-
-console.log(actors)
