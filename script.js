8192c528b1582b6ba4e6b3309a8ad1cff1720ddf
Christoph551 <christophersimmonds551@gmail.com>
2023-01-16 00:17:06 -0700

    Vanilla JS version. Not completed.

--- script.js
+++ script.js
@@ -1,128 +1,52 @@
-const StreamKey = 'f46c29e56amsh9e809a40b041c81p199e23jsne729af91c033'; // Rapid API
 
-const tmbdKey = '1fbb8ee4f6b00e55b99760b9656ba30e'
-    // url: https://api.themoviedb.org/3/movie/550?api_key=1fbb8ee4f6b00e55b99760b9656ba30e
-
-
-    //const MoviePost = `http://img.omdbapi.com/?apikey=${apiKey}&t=${movieTitle}`; see line 61
-
-//variables to tie the html
-const $searchBtn = $('#searchBtn'); // On index.html page
-const $userInput = $('#userInput')
+// Attempted to resolve issues using vanilla syntax. 
+// A team member sorted the issue first, so we'll use that rendition of the code base. 
 
 
-//makes it not case sensitive 
-function compare(a, b) {
-return a.movieName.toUpperCase().localeCompare(b.movieName.toUpperCase());
-}
+const movieButton = document.querySelector('#searchBtn')
+const movieInput = document.querySelector('#userInput')
+const prompt = document.getElementById('modal');
+const promptTxt = document.getElementById('promptTxt');
+const exitPrompt = document.getElementById('close');
 
-// store movies/shows
-function storeMovies(movieName,movieData) {
-    localStorage.setItem(movieName, JSON.stringify(movieData));
-}
-
-function storeStreaming(streamingService, streamingData) {
-    localStorage.setItem(streamingService, JSON.stringify(streamingData));
-}
-
-
-
-$searchBtn.click(function() {
-    const omdbKey = '8b30258'
-    const movieName = $userInput.val();
-    $.ajax({
-        url: `https://www.omdbapi.com/?apikey=${omdbKey}&t=${movieName}`,
-                success: function(data) {
-                    // process the returned data
-                    storeMovies(movieName, data);
-                // storeStreaming(streamingService, data)
-                    window.location.href = "summary.html?name=" + movieName;
-                    // I believe we'll need to add if/else statements. Clicking the button without typing still takes the user to the summary page.
-                    }
-            });
+function searchButton (event) {
+    event.preventDefault();
+    const movieName = movieInput.value.trim();
+    if (movieName == '') {
+        prompt.style.display = ".modalContent";
+        promptTxt.textContent = "Oops! Please be sure to enter the title of a movie or show.";
+        exitPrompt.addEventListener('click', function() {
+            prompt.style.display= `none`;
         })
+        return;
+    } 
+    // window.location.href = "summary.html?name=" + movieName;
+    getMovieData ();
+    // getMovieData();
+}
 
 
 
+function getMovieData () {
+    const movieName = movieInput.value.trim();
+    const omdbKey = '8b30258'
+    const getMovieInfo = `https://www.omdbapi.com/?apikey=${omdbKey}&t=${movieName}`;
+    
+    fetch(getMovieInfo) 
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            const logArrayElements = (element, index /*, array */) => {
+                console.log(`a[${index}] = ${element}`);
+            };
+            // clearData();
+            [2, 5, , 9].forEach(logArrayElements);
+            displayMovieData();
 
-// Functions for the movie, streaming api calls
-
-//function buildURLcity(city) {
-    //if (city) {
-  //      return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-//   }
-//}
-
-// function buildUrlMovie(Movie){
-//     if (Movie){
-//     return `http://www.omdbapi.com/?apikey=${ombdKey}&t=${Movie}`;   
-// }
-// }
-
-// function buildUrlMovImg(poster){ ///this one is ???
-//     if (poster){
-//     return `http://img.omdbapi.com/?apikey=${omdbKey}&t=${Movie}`;
-// }
-// }
-
-//const options = { //this is the stream fetch command 
-	//method: 'GET',
-	//headers: {
-	//	'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-	//	'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-	//}
-//};
-
-//fetch('https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&output_language=en&language=en', options)
-//	.then(response => response.json())
-//	.then(response => console.log(response))
-//	.catch(err => console.error(err));
-
-//function buildUrlStream(stream){
-// if (stream){
-//     return "http"
-//
-
-//
-
-// function buildURLId(id) {
-//     return `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}`;
-
-// })
-
-// Store current city in past cities // make it into movies
-// let city = response.name;
-// let id = response.id;
-// // Remove duplicate cities
-// if (pastCities[0]) {
-//     pastCities = $.grep(pastCities, function (storedCity) {
-//         return id !== storedCity.id;
-//     })
-// }
-// pastCities.unshift({ city, id });
-// storeCities();
-// displayCities(pastCities);
-
-// // Display current weather // make it into movie 
-// cityEl.text(response.name);
-// let formattedDate = moment.unix(response.dt).format('L');
-// dateEl.text(formattedDate);
-// let weatherIcon = response.weather[0].icon;
-// weatherIconEl.attr('src', `http://openweathermap.org/img/wn/${weatherIcon}.png`).attr('alt', response.weather[0].description);
-// temperatureEl.html(((response.main.temp - 273.15) * 1.8 + 32).toFixed(1));
-// humidityEl.text(response.main.humidity);
-// windEl.text((response.wind.speed * 2.237).toFixed(1));
-
-// function displayLastSearchedCity() {
-//     if (pastCities[0]) {
-//         let queryURL = buildURLId(pastCities[0].id);
-//         searchWeather(queryURL);
-//     } else {
-//         // if no past searched cities, load Detroit weather data
-//         let queryURL = buildURLcity("Tucson");
-//         searchWeather(queryURL);
-//     }
-// }
+        })
+    
+}
 
 
-
+movieButton.addEventListener('click', searchButton);
--- summary.js
+++ summary.js
@@ -1,5 +1,30 @@
-// Variables moved from script.js file
-// const $movieResults = $('#movieResults'); 
+// // Variables moved from script.js file
+// // const $movieResults = $('#movieResults'); 
+// const $movieResults = document.getElementById('#movieResults');
 // let pastMovies = []; 
 
 
+
+
+
+
+// const searchData = {
+//     title: $userInput.value,
+//     actors: $actors.value,
+//     genre: $genre.value,
+//     plot: $plot.value.trim(),
+//     rating: $rating.value,
+//     poster: $poster.value,
+// }
+
+console.log(title)
+
+renderMovieData ();
+
+function renderMovieData(data) {
+    const movieData = JSON.parse(localStorage.getItem("Shrek"));
+    for (let i=0; i<movieData.length; i++) {
+        document.getElementById('#movieResults').innerHTML +=
+        data[i].id + data[i.title + ""];
+    }
+}
