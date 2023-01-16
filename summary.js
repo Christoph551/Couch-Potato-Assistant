753bf4647f18eae8c3f84a33bcd1fe8ef3be94b6
Christoph551 <christophersimmonds551@gmail.com>
2023-01-16 13:11:43 -0700

    Added more content from API on summary page

--- summary.js
+++ summary.js
@@ -5,3 +5,7 @@ const movieData = JSON.parse(localStorage.getItem(movieName));
 document.querySelector("#movieTitle").textContent = movieData.Title;
 document.querySelector("#moviePlot").textContent = movieData.Plot;
 document.querySelector("#moviePoster").src = movieData.Poster;
+document.querySelector("#actors").textContent = movieData.Actors;
+document.querySelector('#genre').textContent = movieData.Genre;
+document.querySelector('#rated').textContent = movieData.Rated;
+document.querySelector('#runtime').textContent = movieData.Runtime;
