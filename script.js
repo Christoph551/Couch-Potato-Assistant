393d6fbe05912cdfcba2fa8427fdd60d748c0921
lsegura06 <leosegura006@gmail.com>
2023-01-16 00:23:34 -0700

    api working

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const omdbKey = '8b30258'
 
 //variables to tie the html
 const $searchBtn = $('#searchBtn'); // On index.html page
-const $userInput = $('#userInput')
+const $userInput = $('#userInput');
+
+let lastSearched = []; 
 
 
 //makes it not case sensitive 
@@ -17,20 +19,62 @@ function storeMovies(movieName,movieData) {
     localStorage.setItem(movieName, JSON.stringify(movieData));
 }
 
-$searchBtn.click(function() {
-    const movieName = $userInput.val();
-    $.ajax({
-        url: `https://www.omdbapi.com/?apikey=${omdbKey}&t=${movieName}`,
-                success: function(data) {
-                  // process the returned data
-                storeMovies(movieName,data);
-                    window.location.href = "summary.html?name=" + movieName;
-                    // I believe we'll need to add if/else statements. Clicking the button without typing still takes the user to the summary page.
-                }
-            });
-        });
+// $searchBtn.click(function() {
+//     const movieName = $userInput.val();
+//     $.ajax({
+//         url: `https://www.omdbapi.com/?apikey=${omdbKey}&t=${movieName}`,
+//                 success: function(data) {
+//                   // process the returned data
+//                 storeMovies(movieName,data);
+//                     window.location.href = "summary.html?name=" + movieName;
+//                     // I believe we'll need to add if/else statements. Clicking the button without typing still takes the user to the summary page.
+//                 }
+//             });
+//         });
 
     
+$searchBtn.click(function() {
+    const movieName = $userInput.val();
+    if (movieName.length === 0) {
+        
+        // display an error message to the user
+        alert("Please enter a movie name");
+    } else {
+        $.ajax({
+            url: `https://www.omdbapi.com/?apikey=${omdbKey}&t=${movieName}`,
+            success: function(data) {
+                lastSearched = JSON.parse(localStorage.getItem("lastSearched")) || [];
+                lastSearched.unshift(movieName); // add the movie to the front of the array
+                if (lastSearched.length > 3) {
+                    lastSearched.pop(); // remove the last movie if there are more than 3 in the array
+                }
+                localStorage.setItem("lastSearched", JSON.stringify(lastSearched));
+                updateLastSearched();
+                storeMovies(movieName,data);
+                window.location.href = "summary.html?name=" + movieName;
+            }
+        });
+    }
+});
+
+function updateLastSearched() {
+    lastSearched = JSON.parse(localStorage.getItem("lastSearched")) || [];
+    $('#lastSearched').empty(); // remove any existing buttons
+    for (let i = 0; i < lastSearched.length; i++) {
+        let movie = lastSearched[i];
+        let Moviebutton = $(`<button>${movie}</button>`);
+        Moviebutton.click(function() {
+            // add a click event to the button to search for the movie
+            searchMovie(movie);
+        });
+        $('#lastSearched').append(Moviebutton);
+    }
+}
+
+
+
+        
+        
 
 
 
--- summary.js
+++ summary.js
@@ -1,5 +1,12 @@
 // Variables moved from script.js file
-const $movieResults = $('#movieResults'); 
-let pastMovies = []; 
 
+
+
+
+const movieName = new URLSearchParams(window.location.search).get("name");
+const movieData = JSON.parse(localStorage.getItem(movieName));
+
+document.querySelector("#movieTitle").textContent = movieData.Title;
+document.querySelector("#moviePlot").textContent = movieData.Plot;
+document.querySelector("#moviePoster").src = movieData.Poster;
 
